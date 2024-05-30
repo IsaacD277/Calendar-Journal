3f1e1333d75e6ff78392e37e9ec55914d6730c8c
isaacd2 <idarr08@gmail.com>
2024-05-30 18:22:22 -0400

    Initial Commit

---
+++ Calendar Journal/Calendar_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_JournalApp.swift
+//  Calendar Journal
+//
+//  Created by Isaac D2 on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar Journal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calendar Journal
+//
+//  Created by Isaac D2 on 5/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
