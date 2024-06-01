3827b5783a06d8b0dc51f42d4e9ba682c7512e3a
isaacd2 <idarr08@gmail.com>
2024-06-01 12:40:09 -0400

    Copied from Video

--- Calendar Journal/ContentView.swift
+++ Calendar Journal/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader {
+            let safeArea = $0.safeAreaInsets
+            
+            Home(safeArea: safeArea)
+                .ignoresSafeArea(.container, edges: .top)
         }
-        .padding()
     }
 }
 
